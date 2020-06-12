2c50a9acd94f9d1ed7c3177eb28fe9ef7d27a9f5
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-06-11 21:56:42 -0700

    got array push working for first two variables

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// var possibleChars=[]; need an array of potential chars 
+function generatePassword () {
+// need an array of potential chars, here's all the arrays. 
+var possibleChars=[]; 
+var special = ["~", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p", "q", "r", "s", "t", "u", "v","w","x","y","z"];
 
-// propt user for:
-// pw length (8-128)
-  // store pw length in a variable
-// confirm if want special chars
+// make a toUPPERCAse option for the caps version to spread? will that work?
+
+// TODO: pw length (8-128) store pw length in a variable
+var pwLength = prompt("How long would you like your Password? (accepts 8-128 for length)");
+  // confirm if user wants numbers
+  // if (8<=pwLength<=128) {
+  //   while (i = 0; i <=pwLength; i++) {
+  //     // make this a while loop with randomized pull from array, not for loop
+  //   }
+  // } 
+
+  
+// confirm if user wants special chars
+let specialChars = confirm("Would you like to include special chars?");
   // includesSpecialCharacter
-// confirm if user wants numbers
-  // store in includeNumbers
-// confirm lowercase
-  // store in includeLower
-// confirm upper case 
-  // store in includeUpper
-      // 5 vars so far to store at top of code
+  if (specialChars == true) {
+    possibleChars.push(...special);
+    console.log(possibleChars);
+  }
+  // confirm if user wants number chars
+  let numberChars = confirm("How about Numbers?");
+   if (numberChars == true) {
+     possibleChars.push(...numbers);
+     console.log(possibleChars);
+   }
 
-    // if character boolean is true, add it to array of possible char - investigate the concat method
-    // also look at spread operator [...special, ...upper], so also will need arrays of special chars, numbers, etc
+// TODO:confirm lowercase
+  // TODO:store in includeLower via return?
 
-// look at var specialChars = "!@*%&^#(".split   split is way to moderate a string by splitting it into an array, then .join can put them back together
-// goign to have an Array, and add conditions in based on choices 
+// TODO:confirm upper case 
+  // TODO:store in includeUpper
 
-// when poss char array complete, build new password
-  // generate arrays, pick random from possible, add it to string var passwordStr="", then return passwordStr
+      // TODO:5 vars so far to store STORE via push into possiblechars at top of code, do at confirm each step?
+
+    // TODO:if character **boolean is true**, add it to array of possible char - investigate the concat method
+    // TODO:also look at spread operator [...special, ...upper], so also will need arrays of special chars, numbers, etc
+
+// TODO:look at var specialChars = "!@*%&^#(".split   split is way to moderate a string by splitting it into an array, then .join can put them back together
+//TODO: goign to have an Array, and add conditions in based on choices 
+
+//TODO: when poss char array complete, build new password
+  //TODO: generate arrays, pick random from possible, add it to string var passwordStr="", then return passwordStr
+}
 
 // Write password to the #password input
 function writePassword() {
