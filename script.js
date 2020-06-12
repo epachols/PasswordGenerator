804dc51133dca753b6224aa51d8757c026ea5f65
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-06-11 23:39:27 -0700

    having trouble with edge case, putting it down before fixing the actual loop

--- script.js
+++ script.js
@@ -8,55 +8,56 @@ var special = ["~", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_"];
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p", "q", "r", "s", "t", "u", "v","w","x","y","z"];
 
+var upperLetters = (((letters.join("")).toUpperCase()).split(""));
 // var lettString = letters.join("");
 // var upperLetters = (lettString.toUpperCase()).split("");
 // which turned into ...
 
-var upperLetters = (((letters.join("")).toUpperCase()).split(""));
 
+// WORK NEEDED HERE *******************************************888888888888888
 
-// TODO: pw length (8-128) store pw length in a variable
-var pwLength = prompt("How long would you like your Password? (accepts 8-128 for length)");
-  // confirm if user wants numbers
-  if (8<=pwLength<=128) {
-    while (i = 0; i <=pwLength; i++) {
-      // make this a while loop with randomized pull from array, not for loop
-    }
-  } 
+// TODO: LOOP THE EDGE CASE to include false inputs  pw length (8-128) store pw length in a variable
+let acceptLength = ((pwLength >= 8) && (pwLength <=128));
+do {
+  var pwLength = prompt("How long would you like your Password? (accepts 8-128 for length)"); 
+}
+while (condition);
+//   // let pwLength = prompt ("No really, I need a numerical password length between 8-128 to go anywhere");
+//   // code block to be executed
 
-  
+// WORK NEEDED HERE *******************************************8888888888888888
+
 // confirm if user wants special chars
 
-let specialChars = confirm("Would you like to include special chars?");
-  // includesSpecialCharacter
-  if (specialChars == true) {
-    possibleChars.push(...special);
-    console.log(possibleChars);
-  }
-
-  // confirm if user wants number chars
-let numberChars = confirm("How about Numbers?");
-  if (numberChars == true) {
-    possibleChars.push(...numbers);
-    console.log(possibleChars);
-  }
-
-// confirm user desire for lowercase
-  let lcChars = confirm("Include Lower case english alphabet?");
-    if (lcChars == true) {
-      possibleChars.push(...letters);
+  let specialChars = confirm("Would you like to include special chars?");
+    // includesSpecialCharacter
+    if (specialChars) {
+      possibleChars.push(...special);
       console.log(possibleChars);
     }
 
-// TODO:confirm upper case 
-let ucChars = confirm("Include uper case english alphabet?");
-    if (ucChars == true) {
-        // TODO:store in includeUpper
-        possibleChars.push(...upperLetters);
-        console.log(possibleChars);
+    // confirm if user wants number chars
+  let numberChars = confirm("How about Numbers?");
+    if (numberChars) {
+      possibleChars.push(...numbers);
+      console.log(possibleChars);
     }
 
-      // TODO:5 vars so far to store STORE via push into possiblechars at top of code, do at confirm each step?
+  // confirm user desire for lowercase
+    let lcChars = confirm("Include Lower case english alphabet?");
+      if (lcChars) {
+        possibleChars.push(...letters);
+        console.log(possibleChars);
+      }
+
+  // confirm upper case 
+  let ucChars = confirm("Include uper case english alphabet?");
+      if (ucChars) {
+          // TODO:store in includeUpper
+          possibleChars.push(...upperLetters);
+          console.log(possibleChars);
+      }
+  }
 
     // TODO:also look at spread operator [...special, ...upper], so also will need arrays of special chars, numbers, etc
 
@@ -64,7 +65,7 @@ let ucChars = confirm("Include uper case english alphabet?");
 
 //TODO: when poss char array complete, build new password
   //TODO: generate arrays, pick random from possible, add it to string var passwordStr="", then return passwordStr
-}
+
 
 // Write password to the #password input
 function writePassword() {
